c4f03e8be501bad89d5c302c8261cbc7eb0271de
art-idea5642 <chertezhy.ru@gmail.com>
2024-06-01 19:48:51 +0400

    Initial commit

---
+++ src/main/java/com/art/calculator/controller/CalculatorController.java
@@ -0,0 +1,43 @@
+package com.art.calculator.controller;
+
+import com.art.calculator.service.CalculatorService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/calculator")
+public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
+    public String greetings() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @GetMapping
+    public String sum(int num1, int num2) {
+        return num1+ " + " + num2 + " = " + calculatorService.sum(num1, num2);
+    }
+
+    @GetMapping
+    public String minus(int num1, int num2) {
+        return num1+ " - " + num2 + " = " + calculatorService.minus(num1, num2);
+    }
+
+    @GetMapping
+    public String multiply(int num1, int num2) {
+        return num1+ " * " + num2 + " = " + calculatorService.multiply(num1, num2);
+    }
+
+    @GetMapping
+    public String divide(int num1, int num2) {
+        return num1+ " / " + num2 + " = " + calculatorService.divide(num1, num2);
+    }
+
+}
