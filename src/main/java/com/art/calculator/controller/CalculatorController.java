259556a4b58ff8e5fed39adb80401733aaf7a4e8
art-idea5642 <chertezhy.ru@gmail.com>
2024-10-20 23:21:53 +0400

    Сделано ДЗ

--- src/main/java/com/art/calculator/controller/CalculatorController.java
+++ src/main/java/com/art/calculator/controller/CalculatorController.java
@@ -3,12 +3,12 @@ package com.art.calculator.controller;
 import com.art.calculator.service.CalculatorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
-
     private final CalculatorService calculatorService;
 
     public CalculatorController(CalculatorService calculatorService) {
@@ -16,31 +16,32 @@ public class CalculatorController {
     }
 
     @GetMapping
-    public String greetings() {
+    public String welcome() {
         return "Добро пожаловать в калькулятор";
     }
 
-    @GetMapping
-    public String sum(int num1, int num2) {
-
-        return num1 + " + " + num2 + " = " + calculatorService.sum(num1, num2);
+    @GetMapping("/plus")
+    public String plusNumbers(@RequestParam double num1, @RequestParam double num2){
+        double result = calculatorService.plus(num1, num2);
+        return String.format("%.0f + %.0f = %.0f", num1, num2, result);
     }
 
-    @GetMapping
-    public String minus(int num1, int num2) {
-        return num1 + " - " + num2 + " = " + calculatorService.minus(num1, num2);
+    @GetMapping("/minus")
+    public String minusNumbers(@RequestParam double num1, @RequestParam double num2){
+        double result = calculatorService.minus(num1, num2);
+        return String.format("%.0f - %.0f = %.0f", num1, num2, result);
+    }
+    @GetMapping("/multiply")
+    public String multiplyNumbers(@RequestParam double num1, @RequestParam double num2){
+        double result = calculatorService.multiply(num1, num2);
+        return String.format("%.0f * %.0f = %.0f", num1, num2, result);
     }
 
-    @GetMapping
-    public String multiply(int num1, int num2) {
-        return num1 + " * " + num2 + " = " + calculatorService.multiply(num1, num2);
+    @GetMapping("/divide")
+    public String divideNumbers(@RequestParam double num1, @RequestParam double num2){
+        double result = calculatorService.divide(num1, num2);
+        return String.format("%.0f / %.0f = %.0f", num1, num2, result);
     }
 
-    @GetMapping
-    public String divide(int num1, int num2) {
-        if (num2 == 0) {
-            return "На ноль делить нельзя";
-        }
-        return num1 + " / " + num2 + " = " + calculatorService.divide(num1, num2);
-    }
+
 }
